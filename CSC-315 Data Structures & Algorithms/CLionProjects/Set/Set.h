52772a5a7663f5915f736ef16cde391a8d0a8800
jonahmorgan <jonah.morgan@mines.sdsmt.edu>
2022-09-12 21:34:03 -0600

    Added size function for set(no-class), updated isMember function.

--- CSC-315 Data Structures & Algorithms/CLionProjects/Set/Set.h
+++ CSC-315 Data Structures & Algorithms/CLionProjects/Set/Set.h
@@ -1,23 +1,26 @@
 //
-// Created by jonah on 9/12/2022.
+// Created by Jonah Morgan on 9/12/2022.
 //
 
 #ifndef SET_SET_H
 #define SET_SET_H
 #include <iostream>
 
-
+// Node structure defintion.
 struct node{
     int value;
     node* next;
 };
 
-
+// Definitions for functions.
 void insert(node* &headptr, int val);
+void remove(node* &headptr, int val);
 bool isMember(int val, node* &headptr);
+int size(node* headptr);
 void displaySet(node* &headptr);
 
 
+// Functions with implementation.
 void insert(node* &headptr, int val){
     // Checks if the member is already in the set.
     if( isMember(val, headptr )){
@@ -30,14 +33,20 @@ void insert(node* &headptr, int val){
         std::cout<<"Could not create the new node." << std::endl;
     }
 
+    // New node is now the new first link in the set.
     newNode->value = val;
     newNode->next = headptr;
     headptr = newNode;
+}
+
+
+void remove(node* &headptr, int val){
 
 }
 
 
 bool isMember(int val, node* &headptr){
+    // Traverse set with a temporary node to find value.
     node* temp = headptr;
     while(temp != nullptr){
         if(temp->value == val){
@@ -49,12 +58,30 @@ bool isMember(int val, node* &headptr){
 }
 
 
-void displaySet(node* &headptr){
+int size(node* headptr){
+    int setSize = 0;
+
+    // Traverse set to count amount of nodes.
     node* temp = headptr;
-    while (temp != nullptr){
-        std::cout << temp->value << std::endl;
+    while(temp != nullptr){
+        setSize++;
         temp = temp->next;
     }
+    return setSize;
+}
+
+
+void displaySet(node* &headptr){
+    node* temp = headptr;
+    std::cout << "{";
+    while (temp != nullptr){
+        std::cout << temp->value;
+        temp = temp->next;
+        if(temp != nullptr){
+            std::cout << ", ";
+        }
+    }
+    std::cout << "}" << std::endl;
 }
 
 #endif //SET_SET_H
--- CSC-315 Data Structures & Algorithms/CLionProjects/Set/main.cpp
+++ CSC-315 Data Structures & Algorithms/CLionProjects/Set/main.cpp
@@ -3,17 +3,19 @@
 
 
 int main() {
-    node* headptr;
+    node* headptr = NULL;
 
+    insert(headptr, 235);
     insert(headptr, 0);
     insert(headptr, 1);
     insert(headptr, 4);
-    insert(headptr, 0);
     insert(headptr, 1);
     insert(headptr, 3);
     insert(headptr, 3);
     insert(headptr, -3);
 
+
+    std::cout << "Size: " << size(headptr) << std::endl;
     displaySet(headptr);
 
     return 0;
